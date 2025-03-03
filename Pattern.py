0c56dde05ac0c023cabf5fcb7915877d7d79cc1d
Raj <raj.patel@edeltaes.com>
2025-03-03 18:50:58 +0530

    abc

--- Pattern.py
+++ Pattern.py
@@ -1,11 +1,8 @@
-def print_number_triangle(rows):
-    for i in range(1, rows + 1):
-        for j in range(rows - i):
-            print(" ", end="")
-        for k in range(1, 2 * i):
-            print(k, end="")
-        print()
+rows = 4
+current_number = 1
 
-rows = 3
-print_number_triangle(rows)
-
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(current_number, end=" ")
+        current_number += 1
+    print()
