9c69c3dc4553ad1a838678a15d06559ab8c9000d
Raj <raj.patel@edeltaes.com>
2025-03-04 11:24:09 +0530

    abc

--- ITR(M).py
+++ ITR(M).py
@@ -1,11 +1,13 @@
 roman_numerals = {
     3000:"MMM", 2000: "MM", 1000: "M", 900: "CM", 700: "DCC", 600: "DC", 
     500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL",
-    10: "X", 9: "IX",7: "VII", 5: "V", 4: "IV", 1: "I"
+    10: "X", 9: "IX", 7: "VII", 5: "V", 4: "IV", 1: "I"
 }
+
 while True:
-    try:
-        user_input = int(input("Enter an integer: "))
+    user_input = input("Enter an integer: ")
+    if user_input.isdigit():
+        user_input = int(user_input)
         if 1 <= user_input <= 3999:
             roman_string = ""
             for value, symbol in roman_numerals.items():
@@ -16,5 +18,5 @@ while True:
             break
         else:
             print("Please enter a number between 1 and 3999.")
-    except ValueError:
+    else:
         print("Invalid input. Please enter a valid integer.")
